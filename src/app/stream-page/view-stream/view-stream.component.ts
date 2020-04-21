bfb9d3ace41b6b94e4d85c6eda9f09ad0d747b52
DA-Denis <denys.slabozhanin@dataart.com>
2020-04-21 21:37:52 +0300

    Added chat. Small fixes

--- src/app/stream-page/view-stream/view-stream.component.ts
+++ src/app/stream-page/view-stream/view-stream.component.ts
@@ -1,56 +1,67 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { StreamstatService } from "src/app/streamstat.service";
-import { html5 } from "../../utils/channels";
-import flv from "flv.js";
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {StreamstatService} from 'src/app/streamstat.service';
+import {html5} from '../../utils/channels';
+import {DomSanitizer} from '@angular/platform-browser';
+import flv from 'flv.js';
+
+const URL = 'https://widget.mibbit.com/?settings=38d6da09df7f92010527c3537e00d2e8&server=irc.mibbit.net%3A%2B6697&channel=%23';
+
 @Component({
-  selector: "app-view-stream",
-  templateUrl: "./view-stream.component.html",
-  styleUrls: ["./view-stream.component.scss"]
+  selector: 'app-view-stream',
+  templateUrl: './view-stream.component.html',
+  styleUrls: ['./view-stream.component.scss'],
 })
 export class ViewStreamComponent implements OnInit {
-  stream = "main";
+  stream = 'main';
   stats = {
     main: {},
     kino: {},
-    dev: {}
+    dev: {},
   };
 
   player = null;
 
   subscription = null;
+
   constructor(
     private route: ActivatedRoute,
-    private streamStats: StreamstatService
-  ) {}
+    private streamStats: StreamstatService,
+    private sanitizer: DomSanitizer,
+  ) {
+  }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.stream = params.stream;
       this.initPlayer();
     });
-    this.subscription = this.streamStats.statsSubject.subscribe(stats => {
+    this.subscription = this.streamStats.statsSubject.subscribe((stats) => {
       this.stats = stats;
-      console.log(stats);
     });
+  }
+
+  chat() {
+    const url = `${URL}podkolpakom_${this.stream}`;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
   initPlayer() {
     if (flv.isSupported) {
-      const videoElement = <HTMLMediaElement>document.getElementById("player");
+      const videoElement = document.getElementById('player') as HTMLMediaElement;
       this.player = flv.createPlayer(
         {
-          type: "flv",
+          type: 'flv',
           url: html5[this.stream].link,
           cors: true,
-          isLive: true
+          isLive: true,
         },
         {
           isLive: true,
           enableWorker: false,
           enableStashBuffer: false,
-          fixAudioTimestampGap: false
-        }
+          fixAudioTimestampGap: false,
+        },
       );
       this.player.attachMediaElement(videoElement);
       this.player.load();
--- src/app/streamstat.service.ts
+++ src/app/streamstat.service.ts
@@ -1,40 +1,41 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { reduce, map } from "rxjs/operators";
-import { interval, BehaviorSubject, of, merge, forkJoin } from "rxjs";
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {reduce, map} from 'rxjs/operators';
+import {interval, BehaviorSubject, of, merge, forkJoin} from 'rxjs';
+
 const url = name => `https://stats.klpq.men/api/channels/nms/live/${name}`;
-const channels = ["main", "kino", "dev"];
-import humanizeDuration from "humanize-duration";
+const channels = ['main', 'kino', 'dev'];
+import humanizeDuration from 'humanize-duration';
 
 const fixTime = duration =>
   humanizeDuration(duration * 1000, {
     round: true,
     largest: 2,
-    language: "shortEn",
-    spacer: "",
-    delimiter: ":",
+    language: 'shortEn',
+    spacer: '',
+    delimiter: ':',
     languages: {
       shortEn: {
-        y: "y",
-        mo: "mo",
-        w: "w",
-        d: "d",
-        h: "",
-        m: "",
-        s: "sec",
-        ms: "ms"
-      }
-    }
+        y: 'y',
+        mo: 'mo',
+        w: 'w',
+        d: 'd',
+        h: '',
+        m: '',
+        s: '',
+        ms: 'ms',
+      },
+    },
   });
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root',
 })
 export class StreamstatService {
   stats = {
     main: {},
     kino: {},
-    dev: {}
+    dev: {},
   };
 
   statsSubject = new BehaviorSubject(this.stats);
@@ -43,8 +44,6 @@ export class StreamstatService {
     this.initService();
   }
 
-  getStats() {}
-
   initService() {
     const intevalSource = interval(2000);
     intevalSource.subscribe(n => this.fetchStats());
@@ -52,28 +51,24 @@ export class StreamstatService {
 
   fetchStats() {
     const sources = channels.map(channel => {
-      return this.http
-        .get(url(channel))
-        .pipe(map(resp => ({ ...resp, name: channel })));
+      return this.http.get(url(channel)).pipe(map(resp => ({...resp, name: channel})));
     });
 
-    forkJoin(...sources)
-      .pipe(
-        map(channelsArray => {
-          return channelsArray.reduce((acc, channel) => {
-            return {
-              ...acc,
-              [channel.name]: {
-                ...channel,
-                duration: fixTime(channel.duration)
-              }
-            };
-          }, {});
-        })
-      )
-      .subscribe(data => {
-        this.stats = data;
-        this.statsSubject.next(data);
-      });
+    forkJoin(...sources).pipe(
+      map(channelsArray => {
+        return channelsArray.reduce((acc, channel) => {
+          return {
+            ...acc,
+            [channel.name]: {
+              ...channel,
+              duration: fixTime(channel.duration),
+            },
+          };
+        }, {});
+      }),
+    ).subscribe(data => {
+      this.stats = data;
+      this.statsSubject.next(data);
+    });
   }
 }

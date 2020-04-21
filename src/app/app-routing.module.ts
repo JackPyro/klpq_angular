c635b88b0261e9f22227cf3b02a0f80a6accd842
DA-Denis <denys.slabozhanin@dataart.com>
2020-04-21 21:48:34 +0300

    Fixed chat and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,23 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { IndexPageComponent } from "./index-page/index-page.component";
-import { StreamPageComponent } from "./stream-page/stream-page.component";
-import { ViewStreamComponent } from "./stream-page/view-stream/view-stream.component";
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {IndexPageComponent} from './index-page/index-page.component';
+import {StreamPageComponent} from './stream-page/stream-page.component';
+import {ViewStreamComponent} from './stream-page/view-stream/view-stream.component';
 
 const routes: Routes = [
-  { path: "", component: IndexPageComponent },
+  {path: '', component: IndexPageComponent},
   {
-    path: "stream",
+    path: 'stream',
     children: [
-      { path: "", component: StreamPageComponent },
-      { path: ":stream", component: ViewStreamComponent }
-    ]
-  }
+      {path: '', component: StreamPageComponent},
+      {path: ':stream', component: ViewStreamComponent},
+    ],
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
+  exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { IndexPageComponent } from "./index-page/index-page.component";
-import { StreamPageComponent } from "./stream-page/stream-page.component";
-import { ViewStreamComponent } from "./stream-page/view-stream/view-stream.component";
-import { StreamstatService } from "./streamstat.service";
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {IndexPageComponent} from './index-page/index-page.component';
+import {StreamPageComponent} from './stream-page/stream-page.component';
+import {ViewStreamComponent} from './stream-page/view-stream/view-stream.component';
+import {StreamstatService} from './streamstat.service';
+
 @NgModule({
   declarations: [
     AppComponent,
     IndexPageComponent,
     StreamPageComponent,
-    ViewStreamComponent
+    ViewStreamComponent,
   ],
   providers: [StreamstatService],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
